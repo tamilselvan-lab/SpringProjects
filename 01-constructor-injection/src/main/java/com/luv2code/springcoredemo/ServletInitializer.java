b589df28ffcda2c6258e23c8b032d5a1c24f98bf
tamilselvan-lab <59537913+tamilselvan-lab@users.noreply.github.com>
2024-02-22 18:19:47 +0530

    commit

--- 01-constructor-injection/src/main/java/com/luv2code/springcoredemo/ServletInitializer.java
+++ 01-constructor-injection/src/main/java/com/luv2code/springcoredemo/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.springcoredemo;
+package com.luv2code.springcoredemo;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
--- 01-constructor-injection/src/main/java/com/luv2code/springcoredemo/SpringCoreDemoApplication.java
+++ 01-constructor-injection/src/main/java/com/luv2code/springcoredemo/SpringCoreDemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.springcoredemo;
+package com.luv2code.springcoredemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
